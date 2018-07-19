72555a4a4c4327edeacce93ce16085c3c46e488d
sumi37 <masa.sumimoto@gmail.com>
2018-07-19 18:15:49 +0900

    Rename of lv3 style context

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
+// import '@fortawesome/fontawesome-free/scss/fontawesome.scss';
+// import '@fortawesome/fontawesome-free/scss/solid.scss';
+// import '@fortawesome/fontawesome-free/scss/regular.scss';
 import './scss/index.scss';
 import './js/script';
