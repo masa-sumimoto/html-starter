fb5390a391c58e83658371cecc9bc4b8df4f37c4
sumi37 <masa.sumimoto@gmail.com>
2018-07-22 22:30:02 +0900

    Exchange import js style

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,9 @@
-import $ from 'jquery';
-import 'bootstrap';
-import './script1';
+/* eslint-disable */
+// import $ from 'jquery';
+window.$ = window.jQuery = require('jquery');
+// import 'bootstrap';
+require('bootstrap/dist/js/bootstrap');
+// import './script1';
+require('./script1');
+
+console.log('jQuery v' + $.fn.jquery);
